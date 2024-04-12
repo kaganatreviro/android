0bf0bf48f19b015c5fbc681693ad5efc2539fc99
argenio1 <argenasanbekov@mail.ru>
2024-04-12 14:23:49 +0600

    Добавил: BaseViewModel, BaseFragment

---
+++ core-ui/src/main/java/com/example/core_ui/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.example.core_ui.base
+
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding, VM : BaseViewModel>(@LayoutRes layoutId: Int) :
+    Fragment(layoutId) {
+
+    protected abstract val binding: VB
+    protected abstract val viewModel: VM
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialize()
+        setupListeners()
+        launchObservers()
+    }
+
+    protected open fun initialize() {}
+    protected open fun setupListeners() {}
+    protected open fun launchObservers() {}
+}
