c2490e597f4b2921769b6b03fd9dd4312e324836
argenio1 <argenasanbekov@mail.ru>
2024-04-12 13:54:51 +0600

    migrate to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.agp.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsKotlinJvm) apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "Happy Hours"
 include(":app")
+include(":features")
+include(":features:auth")
+include(":features:auth:presentation")
+include(":features:auth:domain")
+include(":data")
+include(":core-ui")
