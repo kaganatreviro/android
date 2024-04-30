6bb79650e8e6d012608ba5adbb2a95ca6bd03cf1
Ramazan Marov <kaganatreviro@gmail.com>
2024-04-30 11:24:20 +0600

    added check permission fun

--- features/main/presentation/src/main/java/com/example/presentation/ui/fragments/qr_scranner/QRScannerFragment.kt
+++ features/main/presentation/src/main/java/com/example/presentation/ui/fragments/qr_scranner/QRScannerFragment.kt
@@ -1,10 +1,15 @@
 package com.example.presentation.ui.fragments.qr_scranner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -19,6 +24,7 @@ class QRScannerFragment: BaseFragment<QrscannerFragmentBinding,
     override val binding by viewBinding(QrscannerFragmentBinding::bind)
     override val viewModel by viewModels<QRScannerViewModel>()
     private lateinit var zbScanner: ZBarScannerView
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +33,12 @@ class QRScannerFragment: BaseFragment<QrscannerFragmentBinding,
     ): View? {
         zbScanner = ZBarScannerView(requireContext())
         return zbScanner.rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registPermissionListener()
+        checkCameraPermission()
     }
 
     override fun onResume() {
@@ -42,6 +54,32 @@ class QRScannerFragment: BaseFragment<QrscannerFragmentBinding,
 
     override fun handleResult(result: Result?) {
         TODO("Not yet implemented")
+
+    }
+
+    private fun checkCameraPermission() {
+        when{
+            ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED ->{
+                        Toast.makeText(requireContext(), "Camera run", Toast.LENGTH_SHORT).show()
+                    }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) ->{
+                Toast.makeText(requireContext(), "We need your permission", Toast.LENGTH_SHORT).show()
+            }
+
+            else ->{pLauncher.launch(Manifest.permission.CAMERA)}
+        }
+    }
+
+    private fun registPermissionListener(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()){
+            if (it){
+                Toast.makeText(requireContext(), "Camera run", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onBackPressed() {
