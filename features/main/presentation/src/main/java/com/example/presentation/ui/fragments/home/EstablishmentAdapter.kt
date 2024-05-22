e9116e634552339c353a367a1190b038079b6a73
Ramazan Marov <kaganatreviro@gmail.com>
2024-05-22 14:46:46 +0600

    accept changes

--- features/main/presentation/src/main/java/com/example/presentation/ui/fragments/home/EstablishmentAdapter.kt
+++ features/main/presentation/src/main/java/com/example/presentation/ui/fragments/home/EstablishmentAdapter.kt
@@ -1,14 +1,19 @@
 package com.example.presentation.ui.fragments.home
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.core_ui.extensions.loadImageWithGlide
 import com.example.domain.models.EstablishmentDetails
 import com.example.presentation.R
 import com.example.presentation.databinding.ItemEstablishmentBinding
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class EstablishmentAdapter(private val clickListener: ItemClickListener) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -31,6 +36,7 @@ class EstablishmentAdapter(private val clickListener: ItemClickListener) :
 
     override fun getItemCount(): Int = if (items.isEmpty()) 1 else items.size
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is Holder -> {
@@ -55,15 +61,22 @@ class EstablishmentAdapter(private val clickListener: ItemClickListener) :
         lateinit var item: EstablishmentDetails
         var index: Int = 0
 
+        @RequiresApi(Build.VERSION_CODES.O)
         @SuppressLint("SetTextI18n", "CheckResult")
         fun bind(_item: EstablishmentDetails, position: Int) {
             item = _item
             index = position
+            val formatterWithSeconds = DateTimeFormatter.ofPattern("HH:mm:ss")
+            val formatterWithoutSeconds = DateTimeFormatter.ofPattern("HH:mm")
+            val happyHoursStart = LocalTime.parse(item.happyHoursStart, formatterWithSeconds)
+            val happyHoursEnd = LocalTime.parse(item.happyHoursEnd, formatterWithSeconds)
+            happyHoursStart.format(formatterWithoutSeconds)
+            happyHoursEnd.format(formatterWithoutSeconds)
 
             item.apply {
+
                 binding.tvName.text = item.name
-                binding.tvHappyTime.text = "in " +
-                        item.happyHoursStart + " from " + item.happyHoursEnd
+                binding.tvHappyTime.text = "$happyHoursStart - $happyHoursEnd"
                 binding.ivRestImage.loadImageWithGlide(item.logo)
             }
         }
