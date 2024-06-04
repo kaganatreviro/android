e24a2d41a83a7117894ce218e212b0f4422615de
Argen99 <argen1999@mail.ru>
2024-06-04 13:14:06 +0600

    fixed the location permissions

--- features/main/presentation/src/main/java/com/example/presentation/ui/fragments/search/establishments/EstablishmentsFragment.kt
+++ features/main/presentation/src/main/java/com/example/presentation/ui/fragments/search/establishments/EstablishmentsFragment.kt
@@ -1,24 +1,22 @@
 package com.example.presentation.ui.fragments.search.establishments
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.*
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.core_ui.base.BaseFragment
+import com.example.core_ui.extensions.showShortToast
 import com.example.presentation.R
 import com.example.presentation.databinding.FragmentEstablishmentsBinding
 import com.example.presentation.ui.fragments.search.SearchViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.maps.android.ktx.addMarker
 import com.google.maps.android.ktx.awaitMap
 import com.google.maps.android.ktx.awaitMapLoad
@@ -30,22 +28,40 @@ class EstablishmentsFragment : BaseFragment<FragmentEstablishmentsBinding, Searc
     override val viewModel by activityViewModel<SearchViewModel>()
     override fun getViewBinding() = FragmentEstablishmentsBinding.inflate(layoutInflater)
 
+    private var gMap: GoogleMap? = null
+    private var locationPermissionRequest: ActivityResultLauncher<Array<String>>? = null
+
     override fun initialize() {
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
+
+        registPermissionListener()
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.CREATED) {
                 val googleMap = mapFragment.awaitMap()
                 googleMap.awaitMapLoad()
 
-                if (checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
-                } else {
-                    googleMap.isMyLocationEnabled = true
-                }
-                addMarkers(googleMap)
+                setupGoogleMap(googleMap)
             }
         }
+    }
+
+    private fun setupGoogleMap(googleMap: GoogleMap) {
+        gMap = googleMap
+        if (checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            googleMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+        addMarkers(googleMap)
     }
 
     private fun addMarkers(googleMap: GoogleMap) {
@@ -65,6 +81,37 @@ class EstablishmentsFragment : BaseFragment<FragmentEstablishmentsBinding, Searc
                     }
                 }
             }
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun registPermissionListener() {
+        locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    gMap?.isMyLocationEnabled = true
+                }
+
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    gMap?.isMyLocationEnabled = true
+                }
+
+                else -> {
+                    showShortToast("No location access granted")
+                }
+            }
+        }
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        locationPermissionRequest?.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
         )
     }
 }
